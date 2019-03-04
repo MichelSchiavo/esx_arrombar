e05a2e99a07ce36d0a3f5f2d497a1dad5ef8f901
Skerverus <37917676+Skerverus@users.noreply.github.com>
2019-03-03 23:29:55 -0300

    Update sv.lua

--- esx_arrombar/server/sv.lua
+++ esx_arrombar/server/sv.lua
@@ -4,19 +4,6 @@ TriggerEvent('esx:getSharedObject', function(obj)
 	ESX = obj
 end)
 
-RegisterCommand("arrombar", function(source, args, rawCommand)
-	local _source = source
-  local xPlayer  = ESX.GetPlayerFromId(source)
-  
-  TriggerClientEvent('esx_comandos:roubando', _source)
-  TriggerClientEvent('esx_comandos:arrombar', _source)
-  TriggerClientEvent('esx_comandos:startTimer', _source)
-  TriggerClientEvent('esx_comandos:pararRoubo', _source)
-end)
-
-
-
-
 RegisterServerEvent('esx_comandos:alertcops')
 AddEventHandler('esx_comandos:alertcops', function(cx,cy,cz)
 	local source = source
@@ -43,4 +30,30 @@ AddEventHandler('esx_comandos:stopalertcops', function()
 			TriggerClientEvent('esx_comandos:removecopblip', xPlayers[i])
 		end
 	end
+end)
+
+RegisterServerEvent('esx_comandos:registerActivity')
+AddEventHandler('esx_comandos:registerActivity', function(value)
+	activity = value
+	if value == 1 then
+		activitySource = source
+		--Send notification to cops
+		local xPlayers = ESX.GetPlayers()
+		for i=1, #xPlayers, 1 do
+			local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+			if xPlayer.job.name == 'police' then
+				TriggerClientEvent('esx_comandos:setcopnotification', xPlayers[i])
+			end
+		end
+	else
+		activitySource = 0
+	end
+end)
+
+RegisterCommand("arrombar", function(source, args, rawCommand)
+	local _source = source
+  local xPlayer  = ESX.GetPlayerFromId(source)
+  
+  TriggerClientEvent('esx_comandos:arrombar', _source)
+  TriggerClientEvent('esx_comandos:pararRoubo', _source)
 end)
